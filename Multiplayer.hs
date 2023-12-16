2c8e1c6a6ea4ddbccaf7a341379d958d0e34fd96
Yang Dong <flankado@outlook.com>
2023-12-16 16:42:53 -0500

    updates

--- Multiplayer.hs
+++ Multiplayer.hs
@@ -1,15 +1,15 @@
 module MultiPlayer where
 
-import Network.Socket
-import System.IO
-import Control.Exception
+import BoardPrint
 import Control.Concurrent
+import Control.Exception
 import Control.Monad (when)
 import Control.Monad.Fix (fix)
-
-import System.Random (Random (randomRs), RandomGen, getStdGen)
-import Logic
 import Helpers
+import Logic
+import Network.Socket
+import System.IO
+import System.Random (Random (randomRs), RandomGen, getStdGen)
 
 main :: IO ()
 main = do
@@ -17,7 +17,7 @@ main = do
   setSocketOption sock ReuseAddr 1
   bind sock (SockAddrInet 4242 0)
   listen sock 2
-  playerCountVar <- newMVar 0 
+  playerCountVar <- newMVar 0
   chan <- newChan
   _ <- forkIO $ fix $ \loop -> do
     (_, _) <- readChan chan
@@ -32,55 +32,60 @@ mainLoop sock chan playerCountVar msgNum = do
   forkIO (runConn conn chan playerCountVar msgNum)
   mainLoop sock chan playerCountVar $! msgNum + 1
 
-
-runConn :: (Socket, SockAddr) -> Chan Msg -> MVar Int-> Int -> IO ()
+runConn :: (Socket, SockAddr) -> Chan Msg -> MVar Int -> Int -> IO ()
 runConn (sock, _) chan playerCountVar msgNum = do
-    hdl <- socketToHandle sock ReadWriteMode
-    let broadcastToAll msg = do
-          writeChan chan (msgNum, msg)
-          hPutStrLn hdl msg
+  hdl <- socketToHandle sock ReadWriteMode
+  let broadcastToAll msg = do
+        writeChan chan (msgNum, msg)
+        hPutStrLn hdl msg
 
-    hSetBuffering hdl NoBuffering
+  hSetBuffering hdl NoBuffering
 
-    hPutStrLn hdl "~~~~~~~~~~~~~~~\nWelcome to Capture The Mine.\n~~~~~~~~~~~~~~~\n" 
-    hPutStrLn hdl "Please enter your name?"
-    name <- hGetLine hdl
-    broadcastToAll ("--> " ++ name ++ " entered the game.")
+  hPutStrLn hdl "~~~~~~~~~~~~~~~\nWelcome to Capture The Mine.\n~~~~~~~~~~~~~~~\n"
+  hPutStrLn hdl "Please enter your name?"
+  name <- hGetLine hdl
+  broadcastToAll ("--> " ++ name ++ " entered the game.")
 
-    modifyMVar_ playerCountVar (\count -> do
+  modifyMVar_
+    playerCountVar
+    ( \count -> do
         let newCount = count + 1
-        return newCount)
-    
+        return newCount
+    )
+
+  currentCount <- readMVar playerCountVar
+
+  when
+    (currentCount == 2)
+    ( do
+        g <- getStdGen
+        let explored = Helpers.matrixMaker width height Unexplored -- nothing is explored
+        let board = genGame width height (width * height `div` 10) g
+        let boardString = showMatrixWith tile explored
+        broadcastToAll "\nAll players connected; game starting\n."
+        broadcastToAll boardString
+    )
+
+  commLine <- dupChan chan
+
+  -- fork off a thread for reading from the duplicated channel
+  reader <- forkIO $ fix $ \loop -> do
+    (nextNum, line) <- readChan commLine
+    when (msgNum /= nextNum) $ hPutStrLn hdl line
+    loop
+
+  handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+    line <- hGetLine hdl
     currentCount <- readMVar playerCountVar
+    if currentCount == 1
+      then do hPutStrLn hdl " (waiting for one more player...)" >> loop
+      else do
+        case line of
+          -- If an exception is caught, send a message and break the loop
+          "quit" -> hPutStrLn hdl "Bye!"
+          -- else, continue looping.
+          _ -> broadcastToAll (name ++ ": " ++ line) >> loop
 
-    when (currentCount == 2) (do
-      g <- getStdGen
-      let explored = Helpers.matrixMaker width height Unexplored -- nothing is explored
-      let board = genGame width height (width * height `div` 10) g
-      let boardString = showMatrixWith tile explored
-      broadcastToAll "\nAll players connected; game starting\n."
-      broadcastToAll boardString
-      )
-
-
-    commLine <- dupChan chan
-
-    -- fork off a thread for reading from the duplicated channel
-    reader <- forkIO $ fix $ \loop -> do
-        (nextNum, line) <- readChan commLine
-        when (msgNum /= nextNum) $ hPutStrLn hdl line
-        loop
-
-    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
-        line <- hGetLine hdl
-        currentCount <- readMVar playerCountVar
-        if currentCount == 1 then do hPutStrLn hdl " (waiting for one more player...)" >> loop else do
-          case line of
-              -- If an exception is caught, send a message and break the loop
-              "quit" -> hPutStrLn hdl "Bye!"
-              -- else, continue looping.
-              _      -> broadcastToAll (name ++ ": " ++ line) >> loop
-
-    killThread reader                      -- kill after the loop ends
-    broadcastToAll ("<-- " ++ name ++ " left.") -- make a final broadcast
-    hClose hdl                             -- close the handle
+  killThread reader -- kill after the loop ends
+  broadcastToAll ("<-- " ++ name ++ " left.") -- make a final broadcast
+  hClose hdl -- close the handle
